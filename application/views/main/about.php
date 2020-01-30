82cf279a75b9b411196a4c44bedb9726dc6a1d15
suprayetno <syetno119@gmail.com>
2020-01-30 15:09:33 +0700

    first commit

---
+++ application/views/main/about.php
@@ -0,0 +1,37 @@
+<div class="container">
+	<div class="row">
+		<a class="line"><h4 class="margin h1" data-aos="zoom-out" data-aos-duration="500">Tentang Aplikasi</h4></a>
+          <table class="justify" data-aos="zoom-in-down" data-aos-duration="500">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Keterangan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Framework / versi</td>
+                        <td><a href="#" >Codeigniter / <?php echo CI_VERSION; ?></a></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Template Depan</td>
+                        <td><a href="#" >materialize-landing-page</a></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Template cpanel</td>
+                        <td><a href="#" >AdminBSBMaterialDesign</a></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Library Auth</td>
+                        <td><a href="#" >Codeiniger-Ion-Auth</a></td>
+                    </tr>
+                </tbody>
+        </table>
+    </div>
+    </div>
+</div>
